c095b425e45187db6d1ace56021a770ec29f0c16
kengpong36 <kengpong36@gmail.com>
2015-05-06 15:38:30 +0700

    test

--- app/src/main/java/com/xj/ij/projectxfinal/MainActivity.java
+++ app/src/main/java/com/xj/ij/projectxfinal/MainActivity.java
@@ -1,19 +1,52 @@
 package com.xj.ij.projectxfinal;
 
-import android.support.v7.app.ActionBarActivity;
+import java.util.ArrayList;
+
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+public class MainActivity extends Activity {
 
-public class MainActivity extends ActionBarActivity {
+    private ListView listView;
+    private ArrayList<String> list;
+    private ArrayAdapter<String> arrayAdapter;
+    private ConnectServer connectServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //เชื่อม listView กับ View
+        listView = (ListView)findViewById(R.id.listView);
+
+        //สร้างตัวเชื่อมต่อกับ Server ไปที่ URL ที่กำหนด
+        connectServer = new ConnectServer(this, "http://qazx.servehttp.com:99/service/api/restaurant/res/format/json");
+
+
+
+        //เชื่อมต่อกับ Server
+        connectServer.execute();
     }
 
+    //ถ้าดึงข้อมูลจาก Server เสร็จแล้ว จะมาทำงานที่ Function นี้
+    public void setList(ArrayList<String> list){
+        this.list = list;
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, this.list);
+        listView.setAdapter(arrayAdapter);
+    }
 
+    //ถ้าไม่สามารถเชื่อมต่อกับ Server ได้จะมาทำงานที่ Function นี้
+    public void cannotConnectToServer() {
+        Toast.makeText(this, "ไม่สามารถเชื่อมต่อกับ Server", Toast.LENGTH_LONG).show();
+    }
 
+    //ถ้าดึงข้อมูลจาก Server มีปัญหา จะมาทำงานที่ Function นี้
+    public void errorConnectToServer() {
+        Toast.makeText(this, "เกิดขอผิดพลาดในการดึงข้อมูล", Toast.LENGTH_LONG).show();
+    }
 }
